ec9afbc8b8a92cc2540dfd15a2be1ebcdb1855a7
Panwar-Simran <simransaloni2005@gmail.com>
2026-06-01 21:00:47 +0530

    Initial project setup - Spring Boot 3.5.14

---
+++ src/test/java/com/Simran/apartmentmanager/ApartmentmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Simran.apartmentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
