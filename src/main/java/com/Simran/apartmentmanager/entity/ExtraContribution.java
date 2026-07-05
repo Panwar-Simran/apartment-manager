2eadb61c5b3becc4c31d5c2382dd7420527649d4
Panwar-Simran <simransaloni2005@gmail.com>
2026-07-05 20:01:07 +0530

    Implemented ExtraContributiin Repo and Entity

--- src/main/java/com/Simran/apartmentmanager/entity/ExtraContribution.java
+++ src/main/java/com/Simran/apartmentmanager/entity/ExtraContribution.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name="extra_contributions")
 @Data
@@ -15,5 +19,29 @@ public class ExtraContribution {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="approved_by", nullable=false)
+    private User approvedBy;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private LocalDate contributionDate;
+
+    private String proofUrl;
+
+    @Column(nullable = false)
+    private String status;
+
+    private LocalDateTime createdAt;
 
 }
--- src/main/java/com/Simran/apartmentmanager/repository/ExtraContributionRepository.java
+++ src/main/java/com/Simran/apartmentmanager/repository/ExtraContributionRepository.java
@@ -1,4 +1,17 @@
 package com.Simran.apartmentmanager.repository;
 
-public interface ExtraContributionRepository {
+import com.Simran.apartmentmanager.entity.ExtraContribution;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExtraContributionRepository extends JpaRepository<ExtraContribution,Long> {
+
+    // Find all contributions by a specific member
+    List<ExtraContribution>findByUser(Long userId);
+
+    // Find all contributions by status
+    List<ExtraContribution> findByStatus(String status);
 }
