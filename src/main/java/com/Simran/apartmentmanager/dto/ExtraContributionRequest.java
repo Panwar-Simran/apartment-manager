4d041b90ed912c603110dad2616a0133952ab513
Panwar-Simran <simransaloni2005@gmail.com>
2026-07-06 19:18:49 +0530

    Implemented Contribution dtos

--- src/main/java/com/Simran/apartmentmanager/dto/ExtraContributionRequest.java
+++ src/main/java/com/Simran/apartmentmanager/dto/ExtraContributionRequest.java
@@ -1,4 +1,25 @@
 package com.Simran.apartmentmanager.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
 public class ExtraContributionRequest {
+
+    @NotNull(message = "Description is required")
+    private String description;
+
+    @NotNull(message="Amount is required")
+    @DecimalMin(value="0.0",inclusive = false, message = "Amount must be greater than 0")
+    private BigDecimal amount;
+
+    @NotNull(message = "Contribution date is required")
+    private LocalDate contributionDate;
+
+    private String proofUrl;
+
 }
--- src/main/java/com/Simran/apartmentmanager/dto/ExtraContributionResponse.java
+++ src/main/java/com/Simran/apartmentmanager/dto/ExtraContributionResponse.java
@@ -1,4 +1,26 @@
 package com.Simran.apartmentmanager.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ExtraContributionResponse {
+    private Long id;
+    private Long userId;
+    private String memberName;
+    private String flatNumber;
+    private String description;
+    private BigDecimal amount;
+    private LocalDate contributionDate;
+    private String proofUrl;
+    private String status;
+    private String approvedByName;
+    private LocalDateTime createdAt;
 }
